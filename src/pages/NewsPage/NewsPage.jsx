c1cf5785d574dd2bc97f13ddf56fd34a4c3fee6e
n1ghtdev <Extr1m7777@gmail.com>
2019-09-30 23:22:17 +0300

    add NewsRow to MainPage

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 import { NewsRow, SubscribeForm } from '../../components';
 
-const NewsPage = () => {
-  return (
-    <main className="all-news-page">
-      <div className="container">
-        <NewsRow first={true}/>
-        <SubscribeForm />
-        <NewsRow />
-      </div>
-    </main>
-  );
-};
+const NewsPage = () => (
+  <main className="all-news-page">
+    <div className="container">
+      <NewsRow first />
+      <SubscribeForm />
+      <NewsRow />
+    </div>
+  </main>
+);
 
 export default NewsPage;
