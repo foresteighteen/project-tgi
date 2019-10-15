671ace6e9ce3e65528a940130493e7ffac9b0b26
n1ghtdev <Extr1m7777@gmail.com>
2019-10-15 17:03:06 +0300

    Add API data to Certificates Page

--- src/pages/CertificatesPage/CertificateHeroSection/CertificateHeroSection.jsx
+++ src/pages/CertificatesPage/CertificateHeroSection/CertificateHeroSection.jsx
@@ -3,14 +3,12 @@ import { Hero, H1 } from '../../../components';
 
 import './CertificateHeroSection.sass';
 
-const CertificateHeroSection = () => {
-  return (
-    <section id="certificate-hero">
-      <Hero classes="hero__certificate">
-        <H1 text="Сертификаты" />
-      </Hero>
-    </section>
-  );
-};
+const CertificateHeroSection = ({ title }) => (
+  <section id="certificate-hero">
+    <Hero classes="hero__certificate">
+      <H1 text={title} />
+    </Hero>
+  </section>
+);
 
 export default CertificateHeroSection;
