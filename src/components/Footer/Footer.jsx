da14ed4db447ff2e2e49f5ca48f6c94f2b9f05de
n1ghtdev <Extr1m7777@gmail.com>
2019-10-23 15:22:26 +0300

    connect api to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,57 +2,58 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import withPageData from '../../containers/withPageData';
+
 import './Footer.sass';
 
-const Footer = () => {
+const WP_PAGE_ID = 537;
+
+const Footer = ({ pageData, pageLoaded }) => {
+  const { copyrights, menu, address, socials } = pageLoaded && pageData.acf;
   return (
     <footer className="footer-wrap">
       <div className="container">
-        <div className="footer">
-          <div className="footer__copy">
-            <p>Все права защищены ©2019 - ООО «Завод ТеплоГидроИзоляции»</p>
-            <div className="footer__copy__map">
-              <Link href="#">Карта сайта</Link>
-              <Link href="#">Пользовательское соглашение</Link>
-              <Link href="#">Правовая информация</Link>
+        {pageLoaded ? (
+          <div className="footer">
+            <div className="footer__copy">
+              <p>{copyrights}</p>
+              <div className="footer__copy__map">
+                {menu.map(({ item }) => (
+                  <Link href={item.link}>Карта сайта</Link>
+                ))}
+              </div>
+            </div>
+            <div className="footer__contacts">
+              {address.map(({ item }) => (
+                <div className="footer__contacts__item">
+                  <h5 className="footer__subtitle">{item.title}</h5>
+                  <a href="#">{item.subtitle}</a>
+                </div>
+              ))}
+            </div>
+            <div className="footer__social">
+              <div className="footer__social__icons">
+                <a href={socials.telegram} className="footer__social__icon">
+                  <FontAwesomeIcon icon={['fab', 'telegram-plane']} />
+                </a>
+                <a href={socials.facebook} className="footer__social__icon">
+                  <FontAwesomeIcon icon={['fab', 'facebook-f']} />
+                </a>
+                <a href={socials.instagram} className="footer__social__icon">
+                  <FontAwesomeIcon icon={['fab', 'instagram']} />
+                </a>
+              </div>
+              <div className="footer__developer">
+                <span>
+                  Разработка сайта <a href="#">TheLegacy</a>
+                </span>
+              </div>
             </div>
           </div>
-          <div className="footer__contacts">
-            <div className="footer__contacts__item">
-              <h5 className="footer__subtitle">Телефон</h5>
-              <a href="#">+7 (495) 980-10-91</a>
-            </div>
-            <div className="footer__contacts__item">
-              <h5 className="footer__subtitle">Фактический адрес</h5>
-              <a href="#">127238 г.Москва, Дмитровское шоссе д.71Б</a>
-            </div>
-            <div className="footer__contacts__item">
-              <h5 className="footer__subtitle">Почта</h5>
-              <a href="#">info@z-tgi.ru</a>
-            </div>
-          </div>
-          <div className="footer__social">
-            <div className="footer__social__icons">
-              <a href="#" className="footer__social__icon">
-                <FontAwesomeIcon icon={['fab', 'telegram-plane']} />
-              </a>
-              <a href="#" className="footer__social__icon">
-                <FontAwesomeIcon icon={['fab', 'facebook-f']} />
-              </a>
-              <a href="#" className="footer__social__icon">
-                <FontAwesomeIcon icon={['fab', 'instagram']} />
-              </a>
-            </div>
-            <div className="footer__developer">
-              <span>
-                Разработка сайта <a href="#">TheLegacy</a>
-              </span>
-            </div>
-          </div>
-        </div>
+        ) : null}
       </div>
     </footer>
   );
 };
 
-export default Footer;
+export default withPageData(WP_PAGE_ID)(Footer);
